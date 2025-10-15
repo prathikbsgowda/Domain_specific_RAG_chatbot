748a20aee5c64b8c6189cb0e5afc1e15267f3b6a
Prathik B S Gowda <96683588+prathikbsgowda@users.noreply.github.com>
2025-10-15 15:36:46 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
-# app/utils.py
+
 from app.config import CHAT_HISTORY_FILE
 
 def save_chat_history(user_query, answer):
     with open(CHAT_HISTORY_FILE, "a", encoding="utf-8") as f:
         f.write(f"User: {user_query}\nBot: {answer}\n\n")
+
